28ae313dbe7990738b0490c602834727502def40
agape <a741398387@gmail.com>
2021-05-13 14:22:02 +0800

    Add User_Agent

--- wenku8toepub.py
+++ wenku8toepub.py
@@ -87,7 +87,8 @@ class Wenku8ToEpub:
                    'username': username,
                    'password': password}
         headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
         }
         response = requests.request("POST", self.api_login, headers=headers, data=payload)
         html = response.content.decode('gbk')
